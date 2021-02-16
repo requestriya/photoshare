5f754cbdbcc3e807f7f5378f46dbad4ec4d6bccf
SinghIsCoding <theriyasingh3@gmail.com>
2021-02-16 15:59:38 +0530

    .

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kwd7ddgb_6df7^(+9m0cj%vn+md0qrm!)1+_gzfx$2h%!=ps(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'photos-cloud.herokuapp.com']
 
